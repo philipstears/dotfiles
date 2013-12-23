d950a6fe5e56125edeac3d12529d0bddbb9e7761
Philip Stears <philip@driveworks.co.uk>
2013-12-23 22:04:22 +0000

    First crack at a script to actually do something

--- init.js
+++ init.js
@@ -2,7 +2,30 @@ var repos = [
 	"https://github.com/kien/ctrlp.vim"
 ];
 
+var spawn = require("child_process").spawn;
 var path = require("path");
 var bundleDir = path.join(__dirname, ".vim/bundle");
 
-console.log("Bundle dir: " + bundleDir);
+function runNext() {
+	if (repos.length === 0) return;
+
+	var repo = repos.shift();
+
+	var repoName = repo.substr(repo.lastIndexOf("/"));
+
+	var repoDir = path.join(bundleDir, repoName);
+
+	var proc = spawn("git", ["clone", repo, repoDir]);
+
+	proc.stdout.on("data", function(data) {
+		console.log(data);
+	});
+
+	proc.stderr.on("data", function(data) {
+		console.log(data);
+	});
+
+	proc.on("exit", function() {
+		runNext();
+	});
+}
