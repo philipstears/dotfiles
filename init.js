2642ea9e538195d39f2cf2bc4c04176bed34069d
Philip Stears <philip@driveworks.co.uk>
2014-09-07 19:13:51 +0100

    retab

--- init.js
+++ init.js
@@ -1,13 +1,13 @@
 var repos = [
-	"https://github.com/kien/ctrlp.vim",
-	"https://github.com/goatslacker/mango.vim",
-	"https://github.com/ahayman/vim-nodejs-complete",
-	"https://github.com/tpope/vim-surround",
-	"https://github.com/jelera/vim-javascript-syntax",
-	"https://github.com/leafgarland/typescript-vim",
-	"https://github.com/jgdavey/vim-railscasts",
-	"https://github.com/groenewege/vim-less",
-	"https://github.com/clausreinke/typescript-tools",
+    "https://github.com/kien/ctrlp.vim",
+    "https://github.com/goatslacker/mango.vim",
+    "https://github.com/ahayman/vim-nodejs-complete",
+    "https://github.com/tpope/vim-surround",
+    "https://github.com/jelera/vim-javascript-syntax",
+    "https://github.com/leafgarland/typescript-vim",
+    "https://github.com/jgdavey/vim-railscasts",
+    "https://github.com/groenewege/vim-less",
+    "https://github.com/clausreinke/typescript-tools",
     "https://github.com/jimenezrick/vimerl",
 ];
 
@@ -16,27 +16,27 @@ var path = require("path");
 var bundleDir = path.join(__dirname, ".vim/bundle");
 
 function runNext() {
-	if (repos.length === 0) return;
+    if (repos.length === 0) return;
 
-	var repo = repos.shift();
+    var repo = repos.shift();
 
-	var repoName = repo.substr(repo.lastIndexOf("/"));
+    var repoName = repo.substr(repo.lastIndexOf("/"));
 
-	var repoDir = path.join(bundleDir, repoName);
+    var repoDir = path.join(bundleDir, repoName);
 
-	var proc = spawn("git", ["clone", repo, repoDir]);
+    var proc = spawn("git", ["clone", repo, repoDir]);
 
-	proc.stdout.on("data", function(data) {
-		console.log(data.toString("utf8"));
-	});
+    proc.stdout.on("data", function(data) {
+        console.log(data.toString("utf8"));
+    });
 
-	proc.stderr.on("data", function(data) {
-		console.log(data.toString("utf8"));
-	});
+    proc.stderr.on("data", function(data) {
+        console.log(data.toString("utf8"));
+    });
 
-	proc.on("exit", function() {
-		runNext();
-	});
+    proc.on("exit", function() {
+        runNext();
+    });
 }
 
 runNext();
