328dab94fd92e6b332c58ba561c69263ec8b336b
Philip Stears <philip@philipstears.com>
2015-02-12 16:38:00 +0000

    switch to pangloss js so that I can use vim-jsx, add support for jade and stylus

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ var repos = [
     "https://github.com/goatslacker/mango.vim",
     "https://github.com/ahayman/vim-nodejs-complete",
     "https://github.com/tpope/vim-surround",
-    "https://github.com/jelera/vim-javascript-syntax",
+    "https://github.com/pangloss/vim-javascript",
     "https://github.com/leafgarland/typescript-vim",
     "https://github.com/jgdavey/vim-railscasts",
     "https://github.com/groenewege/vim-less",
@@ -14,6 +14,9 @@ var repos = [
     "https://github.com/Lokaltog/vim-powerline",
     "https://github.com/othree/html5.vim",
     "https://github.com/tpope/vim-fugitive",
+    "https://github.com/digitaltoad/vim-jade",
+    "https://github.com/wavded/vim-stylus",
+    "https://github.com/mxw/vim-jsx.git",
 ];
 
 var spawn = require("child_process").spawn;
